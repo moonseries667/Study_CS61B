a341ac3e95ff6f5b65cc0d05f9fc117971ae51dd
MoonSeries <moonseries163@163.com>
2026-09-03 08:03:32 +0800

    modified:   src/DoubleUp.java 	modified:   src/Test.java

--- src/DoubleUp.java
+++ src/DoubleUp.java
@@ -1,4 +1,4 @@
-0public class DoubleUp {
+public class DoubleUp {
    /**
      * Returns a new string where each character of the given string is repeated twice.
      * Example: doubleUp("hello") -> "hheelllloo"
